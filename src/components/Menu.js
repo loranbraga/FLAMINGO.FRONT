d6fc3b021790c79ad4df66ffcaa43e4b6d3868ac
Loran Braga <loranpb@gmail.com>
2021-04-08 12:04:22 -0300

    fix: adjust menu responsive

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { useMediaQuery } from 'react-responsive'
 import { useDispatch, useSelector } from 'react-redux'
 import { AiOutlineSearch } from "react-icons/ai";
 import { useHistory } from "react-router-dom"
@@ -16,6 +17,8 @@ const Menu = () => {
 
   const [search, setSearch] = useState('')
 
+  const isMobile = useMediaQuery({ query: '(max-width: 700px)' });
+
   const dispatch = useDispatch()
   const history = useHistory()
 
@@ -26,10 +29,10 @@ const Menu = () => {
   return (
     <div>
       <Navbar color="light" light expand="md">
-        <Container>
-          <NavbarBrand href="/home" style={{color: "#ff409f"}} className="font-weight-bold">Flamingo</NavbarBrand>
+        <Container >
+          <NavbarBrand href="/home" style={{color: "#ff409f"}} className={isMobile ? "font-weight-bold mx-auto" : "font-weight-bold" }>Flamingo</NavbarBrand>
 
-          <Row>
+          <Row className={isMobile ? "mx-auto" : "" }>
             <Input type="text" style={{width:"85%"}} value={search}
               onChange={ (e) => setSearch(e.target.value)}></Input>
             <Button 
@@ -38,10 +41,10 @@ const Menu = () => {
               onClick={() => history.push(`/posts/${search?search:'_'}`)}
               ><AiOutlineSearch /></Button>
           </Row>
-          <div className="d-flex row">
+          <Row className={isMobile ? "mx-auto" : "" }>
             <Link to={`/posts/${useSelector(state => state.username)}`} style={{color: "#ff409f"}} className="font-weight-bold mr-3">Meu Perfil</Link>
             <Link to="/home" style={{color: "#ff409f"}} className="font-weight-bold" onClick={() => logout()}>Sair</Link>
-          </div>
+          </Row>
 
         </Container>
       </Navbar>
