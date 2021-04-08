74e23b08d3d32f2237dd12998cad26ac049f98de
Loran Braga <loranpb@gmail.com>
2021-04-08 08:00:41 -0300

    fix: adjust api

--- src/config/api.js
+++ src/config/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:3333/',
-  // baseURL: '',
+  // baseURL: 'http://127.0.0.1:3333/',
+  baseURL: process.env.REACT_APP_API || 'https://flamingo-br-api.herokuapp.com/',
 });
 
 api.interceptors.request.use(
